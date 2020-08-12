59c29118104289f9740dccb5a4ecbce1614ffd3e
Isidropere <54987564+Isidropere@users.noreply.github.com>
2020-08-11 21:06:49 -0400

    Proactica1

---
+++ Proyecto1/Proyecto1DiplomadoWebMVCUSAD.Web/Proyecto1_Diplomado_Web_MVC_USAD.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto1_Diplomado_Web_MVC_USAD.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            // return View();
+            return "<html><body>" +"<h1>Universidad Auntonoma de Santo Domingo (UASD)</h1>"
+                +"<p>Diplomado de Desarrollo Web C#, MVC</p>"
+                +"</body></html>";
+        }
+        public string DiplomadoWeb()
+        {
+            return "<html><body>" 
+                +"<h1>Estudiante:</h1>"
+                +"<p>Raul Santos<br>"
+                +"Anthony perez<br>"
+                +"Anthony martinez <br>"
+                +"Sabiel martinez</p>"
+                + "</body></html>";
+        }
+    }
+}
---
+++ Proyecto1/Proyecto1DiplomadoWebMVCUSAD.Web/Proyecto1_Diplomado_Web_MVC_USAD.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto1_Diplomado_Web_MVC_USAD.Web.MvcApplication" Language="C#" %>
