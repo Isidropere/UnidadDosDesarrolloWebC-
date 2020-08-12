cab5a8fcf35bd0d5e582b856e1a6171b5d0788ba
Isidropere <54987564+Isidropere@users.noreply.github.com>
2020-08-11 21:20:55 -0400

    Practica2

---
+++ Proyecto2/Proyecto2DiplomadoWebMVCUSADClub/Proyecto2_Diplomado_Web_MVC_USAD_Club/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto2_Diplomado_Web_MVC_USAD_Club.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Fundadores()
+        {
+            return View();
+        }
+        public ActionResult Campeonatos()
+        {
+            return View();
+        }
+    }
+}
